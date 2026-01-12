46ec605412538b5938bd319b4043a52976bd9da4
rudraupasani <rudraupasani2007@gmail.com>
2026-01-12 17:36:09 +0530

    video/call feature

--- lib/socket.ts
+++ lib/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
 // Connect to the standalone server on port 3001
-export const socket = io("https://filezy.vercel.app", {
+export const socket = io("https://filezy.onrender.com", {
     transports: ["websocket", "polling"],
 });
